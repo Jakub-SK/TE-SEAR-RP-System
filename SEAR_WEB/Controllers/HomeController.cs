2c8f648b883cb15f237c61cab957df1ce1c418b9
NooBXRYAN <howai.ryan@gmail.com>
2026-02-12 07:18:08 +0800

    add back IActionReult Index

--- SEAR_WEB/Controllers/HomeController.cs
+++ SEAR_WEB/Controllers/HomeController.cs
@@ -16,6 +16,10 @@ namespace SEAR_WEB.Controllers
         {
             _sessionCache = sessionCache;
         }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
         public async Task<IActionResult> Privacy()
         {
             return View();
